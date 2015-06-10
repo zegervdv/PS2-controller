a8a48b0e3a8f631454e3d8a348444699de10a2c0
zegervdv <zegervdv@me.com>
2015-06-10 18:00:29 +0200

    Sample 4 ADCs with working USB HID

--- source/main.c
+++ source/main.c
@@ -12,7 +12,7 @@
 #define ADC_L_X  (3) //PA5
 #define USB_BUFFER (int)(5)
 
-__IO  uint8_t sample_value[4] = {0};
+__IO  uint16_t sample_value[4] = {0};
 
 uint8_t Send_Buffer[USB_BUFFER];
 uint8_t PrevXferDone = 1;
@@ -39,32 +39,32 @@ int main(int argc, char const* argv[]) {
     while((DMA_GetFlagStatus(DMA1_FLAG_TC1)) == RESET);
     DMA_ClearFlag(DMA1_FLAG_TC1);
 
-    if (sample_value[ADC_R_Y] > 0x90) {
+    if (sample_value[ADC_R_Y] > 0x400) {
       STM_EVAL_LEDOn(LED3);
     } else {
       STM_EVAL_LEDOff(LED3);
     }
-    if (sample_value[ADC_R_X] > 0x90) {
+    if (sample_value[ADC_R_X] > 0x400) {
       STM_EVAL_LEDOn(LED5);
     } else {
       STM_EVAL_LEDOff(LED5);
     }
-    if (sample_value[ADC_L_Y] > 0x90) {
+    if (sample_value[ADC_L_Y] > 0x400) {
       STM_EVAL_LEDOn(LED6);
     } else {
       STM_EVAL_LEDOff(LED6);
     }
-    if (sample_value[ADC_L_X] > 0x90) {
+    if (sample_value[ADC_L_X] > 0x400) {
       STM_EVAL_LEDOn(LED4);
     } else {
       STM_EVAL_LEDOff(LED4);
     }
 
     if ((PrevXferDone) && (USB_Device_dev.dev.device_status == USB_CONFIGURED)) {
-      Send_Buffer[0] = sample_value[0];
-      Send_Buffer[1] = sample_value[1];
-      Send_Buffer[2] = sample_value[2];
-      Send_Buffer[3] = sample_value[3];
+      Send_Buffer[0] = sample_value[0] >> 4;
+      Send_Buffer[1] = sample_value[1] >> 4;
+      Send_Buffer[2] = sample_value[2] >> 4;
+      Send_Buffer[3] = sample_value[3] >> 4;
 
       Send_Buffer[USB_BUFFER - 1] = 0x00;
 
@@ -85,46 +85,49 @@ void init_ADC() {
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
   RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
 
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_1 | GPIO_Pin_0;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
   GPIO_Init(GPIOA, &GPIO_InitStructure);
 
   ADC_StructInit(&ADC_InitStructure);
-  ADC_InitStructure.ADC_Resolution = ADC_Resolution_8b;
+  ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;
   ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
   ADC_InitStructure.ADC_ScanDirection = ADC_ScanDirection_Upward;
   ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
   ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T1_TRGO;
   ADC_Init(ADC1, &ADC_InitStructure);
 
-  ADC_ChannelConfig(ADC1, ADC_Channel_4, ADC_SampleTime_7_5Cycles);
-  /* ADC_ChannelConfig(ADC1, ADC_Channel_5, ADC_SampleTime_7_5Cycles); */
+  ADC_ChannelConfig(ADC1, ADC_Channel_4, ADC_SampleTime_239_5Cycles);
+  ADC_ChannelConfig(ADC1, ADC_Channel_5, ADC_SampleTime_239_5Cycles);
+  ADC_ChannelConfig(ADC1, ADC_Channel_1, ADC_SampleTime_239_5Cycles);
+  ADC_ChannelConfig(ADC1, ADC_Channel_0, ADC_SampleTime_239_5Cycles);
 
   ADC_GetCalibrationFactor(ADC1);
 
   ADC_DMARequestModeConfig(ADC1, ADC_DMAMode_Circular);
-  ADC_Cmd(ADC1, ENABLE);
-  ADC_DMACmd(ADC1, ENABLE);
-
-  while (!ADC_GetFlagStatus(ADC1, ADC_FLAG_ADRDY));
-  ADC_StartOfConversion(ADC1);
 
   RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
 
   DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&ADC1->DR;
-  DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)sample_value;
+  DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)&sample_value[0];
   DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
-  DMA_InitStructure.DMA_BufferSize = 1;
+  DMA_InitStructure.DMA_BufferSize = 4;
   DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
-  DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Disable;
-  DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
-  DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+  DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+  DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+  DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
   DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
   DMA_InitStructure.DMA_Priority = DMA_Priority_High;
   DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
   DMA_Init(DMA1_Channel1, &DMA_InitStructure);
+
+  ADC_Cmd(ADC1, ENABLE);
+  ADC_DMACmd(ADC1, ENABLE);
+
   DMA_Cmd(DMA1_Channel1, ENABLE);
 
+  while (!ADC_GetFlagStatus(ADC1, ADC_FLAG_ADRDY));
+  ADC_StartOfConversion(ADC1);
 }
 
